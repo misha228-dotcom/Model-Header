84c8d01c4fcc702afddce91d0944e346b28e6643
misha228-dotcom <misha07052013@gmailcom>
2026-09-23 08:12:33 +0300

    modal with js

---
+++ script.js
@@ -0,0 +1,47 @@
+(() => {
+      const mobileMenu = document.querySelector('.js-menu-container');
+      const openMenuBtn = document.querySelector('.js-open-menu');
+      const closeMenuBtn = document.querySelector('.js-close-menu');
+      const body = document.querySelector('body');
+
+      const toggleMenu = () => {
+        const isMenuOpen =
+          openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+        openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+        mobileMenu.classList.toggle('is-open');
+
+        // const scrollLockMethod = !isMenuOpen
+        //   ? 'disableBodyScroll'
+        //   : 'enableBodyScroll';
+        // bodyScrollLock[scrollLockMethod](document.body);
+        body.classList.toggle('no-scroll');
+      };
+
+      openMenuBtn.addEventListener('click', toggleMenu);
+      closeMenuBtn.addEventListener('click', toggleMenu);
+
+      // Закрываем мобильное меню на более широких экранах
+      // в случае изменения ориентации устройства.
+      window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+        if (!e.matches) return;
+        mobileMenu.classList.remove('is-open');
+        openMenuBtn.setAttribute('aria-expanded', false);
+        // bodyScrollLock.enableBodyScroll(document.body);
+        body.classList.remove('no-scroll');
+      });
+
+      // Закриваємо меню при кліку на пункт 
+
+      const navLinks = document.querySelectorAll('.mobile-menu a'); 
+
+      navLinks.forEach(link => { 
+        link.addEventListener('click', (e) => { 
+
+          e.preventDefault()
+          mobileMenu.classList.remove('is-open'); 
+        }); 
+      });
+
+
+
+    })();
